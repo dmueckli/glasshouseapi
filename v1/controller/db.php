dafad7ed8d3d0adedab06df7e7392d0e1d4a9c64
dmueckli <muecklich.dennis@gmail.com>
2022-04-17 22:02:32 +0200

    Folder Structures

--- v1/controller/db.php
+++ v1/controller/db.php
@@ -1,17 +1,17 @@
 <?php
 
-String $dbuser = 'glasshouseapi';
-String $dbpassword = 'glasshouseapi';
+
 
 class DB
 {
     private static $writeDBConnection;
     private static $readDBConnection;
 
-    public static function connectWriteDB()
+    public static function connectWriteDB()    
     {
+        
         if (self::$writeDBConnection === null) {
-            self::$writeDBConnection = new PDO('mysql:host=localhost;dbname=tasksdb;charset=utf8', $dbuser, $dbpassword);
+            self::$writeDBConnection = new PDO('mysql:host=localhost;dbname=tasksdb;charset=utf8', 'glasshouseapi', 'qrbaKpxR9TLpAc9V');
             self::$writeDBConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             self::$writeDBConnection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
         }
---
+++ v1/model/response.php
@@ -0,0 +1,20 @@
+<?php
+
+class Response
+{
+    private int $_success;
+    private int $_httpStatusCode;
+    private $_messages = array();
+    private $_data;
+    private bool $_toCache = false;
+    private $_responseData = array();
+
+    public function setSuccess($success)
+    {
+        # code...
+
+    }
+
+
+
+}
